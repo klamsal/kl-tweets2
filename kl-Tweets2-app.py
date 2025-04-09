676f0e77b0a3d23d3e5077aee83de6232da16a95
klamsal <kamallamsal@gmail.com>
2025-04-09 10:58:46 -0500

    Update kl-Tweets2-app.py

--- kl-Tweets2-app.py
+++ kl-Tweets2-app.py
@@ -1,33 +1,36 @@
-import streamlit as st
 import pandas as pd
-import plotly.express as px
-import seaborn as sns
-import matplotlib.pyplot as plt
 
-# Load the dataset
 tweets = pd.read_csv('https://raw.githubusercontent.com/klamsal/kl-tweets2/refs/heads/main/Tweets.csv')
 
-# Streamlit App title
-st.title("Airline Tweets Sentiment Analysis")
+import plotly.express as px
 
-# Pie chart for sentiment counts
-st.header("Count of Tweets by Sentiment")
 sentiment_counts = tweets['airline_sentiment'].value_counts().reset_index()
 sentiment_counts.columns = ['sentiment', 'count']
 
-fig_pie = px.pie(sentiment_counts, values='count', names='sentiment', title="Count of tweets by Sentiment")
-st.plotly_chart(fig_pie)
+fig = px.pie(sentiment_counts,
+             values = 'count',
+             names = 'sentiment',
+             title = "Count of tweets by Sentiment"
+             )
+fig.show()
 
-# Bar graph for airline counts
-st.header("Tweet Counts by Airlines")
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Calculate tweets counts by airlines
 airline_counts = tweets['airline'].value_counts().reset_index()
 airline_counts.columns = ['airline', 'count']
 
+# Create a bargraph using Seaborn
 plt.figure(figsize=(10, 6))
-sns.barplot(x='airline', y='count', palette='pastel', data=airline_counts)
-plt.title('Tweet Counts by Airlines')
+sns.barplot(x='airline', y='count', hue = 'airline',palette = 'pastel', legend = False, data=airline_counts)
+
+# Add titles and labels
+plt.title('Tweet counts by Airlines')
 plt.xlabel('Airline')
 plt.ylabel('Count')
-plt.tight_layout()
 
-st.pyplot(plt)
+
+plt.tight_layout()
+plt.show()
